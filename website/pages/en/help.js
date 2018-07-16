18a87df865f1e547714b552b8eec309c83a42d0a
Jason Hsu <jhsu802701@jasonhsu.com>
2018-07-16 04:55:05 +0000

    Added email address

--- website/pages/en/help.js
+++ website/pages/en/help.js
@@ -20,33 +20,26 @@ function docUrl(doc, language) {
 class Help extends React.Component {
   render() {
     let language = this.props.language || '';
-    const supportLinks = [
-      {
-        content: `Learn more using the [documentation on this site.](${docUrl(
-          'doc1.html',
-          language
-        )})`,
-        title: 'Browse Docs',
-      },
-      {
-        content: 'Ask questions about the documentation and project',
-        title: 'Join the community',
-      },
-      {
-        content: "Find out what's new with this project",
-        title: 'Stay up to date',
-      },
-    ];
 
     return (
       <div className="docMainWrapper wrapper">
         <Container className="mainContainer documentContainer postContainer">
           <div className="post">
             <header className="postHeader">
-              <h1>Need help?</h1>
+              <h1>Help</h1>
             </header>
-            <p>This project is maintained by a dedicated group of people.</p>
-            <GridBlock contents={supportLinks} layout="threeColumn" />
+            
+            <h2>FAQ</h2>
+            Common questions are answered on the <a href="http://www.rubyonracetracks.com/docs/virtualbox.html">Ruby on Racetracks</a> site.
+            
+            <h2>Discuss</h2>
+            <ul>
+              <li><a href='https://groups.google.com/forum/#!forum/ruby_on_racetracks'>Google Group</a></li>
+              <li><a href='https://twitter.com/RubyRacetracks'>Twitter</a></li>
+            </ul>
+            
+            <h2>Email</h2>
+            <img src='../img/email.jpg'/>
           </div>
         </Container>
       </div>
